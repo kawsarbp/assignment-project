d976be0d05b2206acec593983f14ffb0167f084e
Kawsar Ahmed Ashif <96182258+kawsarbp@users.noreply.github.com>
2024-01-11 12:43:02 +0600

    layout done

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>world wide services</title>
+    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+{{--<marquee behavior="" direction=""> || FAST <span>☆</span> RELIABLE <span>☆</span> TRUSTED <span>☆</span> LOW COST <span>☆</span> BANK DEPOSIT <span>☆</span> CASH PICKUP <span>☆</span>  BKASH <span>☆</span> || FAST <span>☆</span> RELIABLE <span>☆</span> TRUSTED <span>☆</span> LOW COST    || FAST <span>☆</span> RELIABLE <span>☆</span> TRUSTED <span>☆</span> LOW COST <span>☆</span> BANK DEPOSIT <span>☆</span> CASH PICKUP <span>☆</span>  Bkash <span>☆</span>h || FAST <span>☆</span> RELIABLE <span>☆</span> TRUSTED <span>☆</span> LOW COST  </marquee>--}}
+
+<div class="row mx-0">
+    <div class="col-md-12 p-0">
+
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <h6> || FAST <">☆</span> </h6>
+                </div>
+                <div class="slide">
+                    <h6>RELIABLE <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>TRUSTED <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>LOW COST <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>BANK DEPOSIT <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>CASH PICKUP <span> ☆ </span></h6>
+                </div>
+                <div class="slide">
+                    <h6> Bkash</h6>
+                </div>
+                <div class="slide">
+                    <h6>|| FAST <span> ☆ <span> </span></span></h6>
+                </div>
+                <div class="slide">
+                    <h6>RELIABLE <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>TRUSTED <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>LOW COST <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>BANK DEPOSIT <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>CASH PICKUP <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>Bkash</h6>
+                </div>
+                <div class="slide">
+                    <h6>|| FAST <span>☆</span> </h6>
+                </div>
+                <div class="slide">
+                    <h6>RELIABLE <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>TRUSTED <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>LOW COST <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>BANK DEPOSIT <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>CASH PICKUP <span> ☆ </span></h6>
+                </div>
+                <div class="slide">
+                    <h6> Bkash</h6>
+                </div>
+                <div class="slide">
+                    <h6>|| FAST <span>☆</span> </h6>
+                </div>
+                <div class="slide">
+                    <h6>RELIABLE <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>TRUSTED <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>LOW COST <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>BANK DEPOSIT <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>CASH PICKUP  <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>Bkash</h6>
+                </div>
+                <div class="slide">
+                    <h6>|| FAST <span>☆</span> </h6>
+                </div>
+                <div class="slide">
+                    <h6>RELIABLE <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>TRUSTED <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>LOW COST <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>BANK DEPOSIT <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>CASH PICKUP <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6> Bkash</h6>
+                </div>
+                <div class="slide">
+                    <h6>|| FAST <span>☆</span> </h6>
+                </div>
+                <div class="slide">
+                    <h6>RELIABLE <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>TRUSTED <span>☆</span></h6>
+                </div>
+
+                <div class="slide">
+                    <h6>LOW COST <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>BANK DEPOSIT <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6>CASH PICKUP <span>☆</span></h6>
+                </div>
+                <div class="slide">
+                    <h6> Bkash</h6>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<nav class="navbar navbar-expand-md md-body-tertiary">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="{{ asset('assets/img/logo.png') }}" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item mx-1">
+                    <a class="nav-link  mynav active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item mx-1">
+                    <a class="nav-link mynav " aria-current="page" href="#">About</a>
+                </li>
+                <li class="nav-item mx-1">
+                    <a class="nav-link mynav " aria-current="page" href="#">Service</a>
+                </li>
+                <li class="nav-item mx-1">
+                    <a class="nav-link mynav " aria-current="page" href="#">Contact</a>
+                </li>
+                <li class="nav-item mx-1">
+                    <a class="nav-link login-btn" aria-current="page" href="#">Login / Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+</html>
